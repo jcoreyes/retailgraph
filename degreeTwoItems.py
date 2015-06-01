7aface4bc5ecdc6915856268b49a9f30f88d7d03
John Co-Reyes <jcoreyes@caltech.edu>
2015-06-01 16:21:11 -0700

    Added graphing items that are close and move

--- degreeTwoItems.py
+++ degreeTwoItems.py
@@ -11,10 +11,14 @@ data = pd.read_csv(sys.argv[1])
 item_map = {}
 item_map_c = {}
 G = nx.Graph()
-items = list(pd.unique(data['item_number']))
-
-for a in items: 
-    G.add_node(a)
+with open("moving_items.pickle", "r") as f:
+    moving_items = pickle.load(f)
+#items = list(pd.unique(data['item_number']))
+items = [x for x in moving_items.keys() if moving_items[x] > 0]
+# print items
+# print len(items)
+# for a in items: 
+#     G.add_node(a)
 
 pairs = list(itools.combinations(items, 2))
 for pair in pairs:
@@ -38,7 +42,7 @@ def vote(id1, id2):
         a = id2
         b = id1
     #antenna_map_c[(a, b)] += 1
-    item_map[(a, b)] += float(a*b)
+    item_map[(a, b)] += 1
 
 
 data = data[data['count'] > 200]
@@ -49,7 +53,8 @@ for name, group in byItem:
         # print group
         for pair in list(itools.combinations(list(set(group['item_number'])), 2)):
             a, b = pair
-            vote(a, b)
+            if pair in item_map:
+                vote(a, b)
 
 for pair in pairs:
     id1, id2 = pair 
@@ -60,6 +65,8 @@ for pair in pairs:
         a = id2
         b = id1
     if item_map[(a, b)] >= 5:
+        G.add_node(a)
+        G.add_node(b)
         # print 'pair: (%d, %d) with %d connections' % (a, b, antenna_map[(a, b)])
         G.add_edge(a, b, weight=(1.0 / item_map[(a, b)]))
 #pickle.dump(antenna_map, "antenna_map.pickle")
@@ -67,7 +74,7 @@ for pair in pairs:
 pos = nx.graphviz_layout(G)
 
 day = sys.argv[1].split(".")[0][-1]
-title = "Relative map of antennas using counts > 200 and day %s data" %day
+title = "Items that are close and move from day %s data" %day
 nx.draw_networkx(G, pos, title=title, node_size=400)
 plt.axis('off')
 plt.title(title)
