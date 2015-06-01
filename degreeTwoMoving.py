0aabac3d2ee16e8e3547e5805e2b9eedfd5dc9e3
Aakash Indurkhya <aindurkh@caltech.edu>
2015-06-01 15:55:31 -0700

    adding pickles and degreeTwoMoving file

--- degreeTwoMoving.py
+++ degreeTwoMoving.py
@@ -6,10 +6,14 @@ import pandas as pd
 import itertools as itools
 import networkx as nx 
 import sys
+import pickle
+from datetime import datetime
 data = pd.read_csv(sys.argv[1])
 antenna_map = {}
+items_moving = {}
 G = nx.Graph()
 antennas = list(pd.unique(data['antenna_id']))
+items = list(pd.unique(data['item_number']))
 
 # Create graph by adding one vertex for each antenna
 for a in antennas: 
@@ -27,6 +31,10 @@ for pair in pairs:
 		a = id2
 		b = id1
 	antenna_map[(a, b)] = 0
+
+# create hash table for each item
+for i in items:
+	items_moving[i] = 0
 
 # print antenna_map
 
@@ -49,17 +57,30 @@ def voteWeight(a, b):
 	else: 
 		return a
 
+FMT = '%Y-%m-%d %H:%M:%S'
+startOfDay = datetime.strptime('2015-02-13 00:00:00', FMT)
+timeVar = []
 # Read in and filter data
 data = data[data['count'] > 200]
+print data
 byItem = data.groupby(['item_number', 'serial_number']) # , 'antenna_id'])
 for name, group in byItem:
-	if len(group['item_number']) > 1:
+	if len(group['item_number']) > 4:
 		# print name 
 		# print group
 		aid = list(group['antenna_id'])
 		counts = list(group['count'])
-		countVals = {}
+		times = list(group['first_seen'])
+		
+		times = [(datetime.strptime(t, FMT) - startOfDay).seconds/3600.0 for t in times]
+		timeVar.append(np.var(times))
 
+		item = list(group['item_number'])[0]
+		if np.var(times) > .2: 
+			print item
+			items_moving[item] += np.var(times)
+
+		countVals = {}
 		for i in range(len(aid)):
 			countVals[aid[i]] = counts[i]
 
@@ -67,6 +88,18 @@ for name, group in byItem:
 		for pair in list(itools.combinations(list(set(group['antenna_id'])), 2)):
 			a, b = pair
 			vote(a, b, voteWeight(countVals[a], countVals[b]))
+
+print np.mean(timeVar)
+timeVar = timeVar - np.mean(timeVar)
+plt.hist(timeVar, bins=25)
+plt.plot()
+plt.show()
+
+
+print items_moving
+
+with open('moving_items.pickle', 'w') as f:
+	pickle.dump(items_moving, f)
 
 # add an edge for pairs with a high enough number of votes
 for pair in pairs:
@@ -81,17 +114,14 @@ for pair in pairs:
 		# print 'pair: (%d, %d) with %d connections' % (a, b, antenna_map[(a, b)])
 		G.add_edge(a, b, weight=(1.0 / antenna_map[(a, b)]))
 
-print pos
+# print pos
 
-# Display the network
-pos = nx.graphviz_layout(G)
+# # Display the network
+# pos = nx.graphviz_layout(G)
 
-day = sys.argv[1].split(".")[0][-1]
-title = "Relative map of antennas using counts > 200 and day %s data" %day
-nx.draw_networkx(G, pos, title=title)
-plt.axis('off')
-plt.title(title)
-plt.show()
-
-with open("antenna_map.pickle", 'w') as f:	
-	pickle.dump(antenna_map, f)
+# day = sys.argv[1].split(".")[0][-1]
+# title = "Relative map of antennas using counts > 200 and day %s data" %day
+# nx.draw_networkx(G, pos, title=title)
+# plt.axis('off')
+# plt.title(title)
+# plt.show()
